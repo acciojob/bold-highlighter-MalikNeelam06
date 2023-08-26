2ac3283c994897cbbc330d5f13372be1a880c859
MalikNeelam06 <128276286+MalikNeelam06@users.noreply.github.com>
2023-08-26 15:45:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-let para=document.getElementsByTagName("strong");
-
+let paraBold;
+window.onload=getBold_items();
+function getBold_items(){
+ paraBold=document.getElementsByTagName("strong");
+}
 function highlight() {
     //Write your code here
-	para.style.color="green"
-
+	for(let i=0;i<paraBold.length;i++){
+	paraBold[i].style.color="green";
+	}
 
 }
 
 
 function return_normal() {
     //Write your code here
-	para.style.color="black";
+	for(let i=0;i<paraBold.length;i++){
+	paraBold[i].style.color="black";
+	}
+	// para.style.color="black";
 
     
 }
